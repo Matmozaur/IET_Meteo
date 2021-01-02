e8a04dfebb100ee3b5a2abe6dd7e3170f44a214a
matmozaur <matmozaur@gmail.com>
2021-01-03 00:24:30 +0100

    update

--- meteoApp/urls.py
+++ meteoApp/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index),
+    path('', views.index, name='index'),
     path('basic_plot/', views.basic_plot, name='basic_plot'),
     path('add_trend/', views.add_trend, name='add_trend'),
     path('add_stats/', views.add_stats, name='add_stats'),
